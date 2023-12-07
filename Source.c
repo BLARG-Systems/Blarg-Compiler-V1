4d547914462dbbcefd0ee18d260fcad6d44b8bca
ASM Machine <ASM Machine@DESKTOP-79RP468>
2023-12-07 13:15:20 -0700

    Add project files.

---
+++ Source.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// cl Source.c /Fe:blargify.exe
+
+int main(int argc, char* argv[]) {
+	if (argc > 1) {
+		char* filename = argv[1];
+		// Check if file ends in .blarg
+		int dot = strrchr(filename, '.');
+		if (!dot || strcmp(dot, ".blarg")) {
+			printf("File '%s' is not a .blarg file.", filename);
+			exit(-1);
+		}
+
+		// Get file and verify access
+		FILE* fp = fopen(filename, "r");
+
+		if (fp != NULL)
+		{
+			// File exists and is readable
+			fseek(fp, 0L, SEEK_END);
+			int size = ftell(fp);
+
+			char* srcContent;
+			srcContent = (char*)malloc(size);
+
+			rewind(fp);
+
+			fgets(srcContent, size, fp);
+
+			printf(srcContent);
+
+			fclose(fp);
+		}
+		else {
+			printf("Unable to access BLARG! source file '%s'", filename);
+			exit(-1);
+		}
+	}
+	else {
+		printf("No file argument supplied.");
+		exit(-1);
+	}
+}
