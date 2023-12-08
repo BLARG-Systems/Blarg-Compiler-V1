529fa9c8cccf0abdc35013cc94d9632e1ef1ef14
Computercodes101 <computercodes101@gmail.com>
2023-12-07 22:18:42 -0700

    Fixed some issues in concatation of strings

--- Source.c
+++ Source.c
@@ -6,6 +6,7 @@
 int main(int argc, char* argv[]) {
 	if (argc > 1) {
 		char* filename = argv[1];
+
 		// Check if file ends in .blarg
 		int dot = strrchr(filename, '.');
 		if (!dot || strcmp(dot, ".blarg")) {
@@ -16,30 +17,35 @@ int main(int argc, char* argv[]) {
 		// Get file and verify access
 		FILE* fp = fopen(filename, "r");
 
-		if (fp != NULL)
-		{
-			// File exists and is readable
+		if (fp != NULL) { // File exists and is readable
 			fseek(fp, 0L, SEEK_END);
-			int size = ftell(fp);
+
+			// Get size of file to allocate a large enough string
+			int size = ftell(fp); 
 
 			char* srcContent;
-			srcContent = (char*)malloc(size);
+			srcContent = (char*) malloc(size * sizeof(char));
 
-			rewind(fp);
+			// Move pointer back to beginning of file
+			rewind(fp); 
 
 			char srcTemp[65535];
 
+			// Loop Through all lines in file, store to srcContent
 			while (fgets(srcTemp, size, fp)) {
+				// TODO: Optimize memory usage while reading lines
+
 				strcat(srcContent, srcTemp);
-				printf("%s", srcTemp);
+				//printf("%s", srcTemp);
 			}
 
-			printf("\n\nFULL STRING:\n");
-			printf("%s", srcContent);
+			printf("FILE AS STRING:\n");
+			printf("%s\n\n", srcContent);
 
+			// Close file
 			fclose(fp);
 		}
-		else {
+		else { // File does not exist or cannot be read from
 			printf("Unable to access BLARG! source file '%s'", filename);
 			exit(-1);
 		}
