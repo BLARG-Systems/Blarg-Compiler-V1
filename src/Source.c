423498ce0b387c59f9765bf358ab1d73fbb3474a
Computercodes101 <computercodes101@gmail.com>
2024-03-12 12:33:53 -0600

    Implement advisor's smarter readfile code

--- src/Source.c
+++ src/Source.c
@@ -7,6 +7,8 @@
 
 // cl src\Source.c /Fe:blargify.exe
 
+#define BUFSIZE 32
+
 int filelen;
 
 char* readfile(char* filename) {
@@ -15,21 +17,23 @@ char* readfile(char* filename) {
 
 
 	if (fp != NULL) { // File exists and is readable
-		// Go to end of file, read length, then return to beginning of file
-		fseek(fp, 0L, SEEK_END);
-		filelen = ftell(fp) - 6; // Again, this is confusing me, but if fixes it!
-		fseek(fp, 0, SEEK_SET);
+		char buffer[BUFSIZE * sizeof(char)];
+		char* output = NULL;
+		int total = 0;
 
-		// Reserve memory for file string
-		char* srcBuffer = (char*)malloc(filelen);
-
-		// Read file into srcBuffer, append EOF indicator
-		fread(srcBuffer, 1, filelen, fp);
-		srcBuffer[filelen] = '\0';
-
+		while (!feof(fp)) {  // Until the end of the file
+			int read = fread(buffer, sizeof(char), BUFSIZE, fp);  // Read the next BUFSIZE characters from the file
+			output = (char*)realloc(output, total + read);   // Note:  skipping error handling from realloc!
+			memcpy(output + total, buffer, read);  // Copy what was read to the end of the output buffer
+			total += read;  // Keep track of the total read
+		}
 		fclose(fp);
 
-		return srcBuffer;
+		output = (char*)realloc(output, total + 1);   // Note:  skipping error handling from realloc!
+		output[total] = '\0';   // Null-terminate the string
+		filelen = strlen(output);
+
+		return output;
 	}
 	else { // File does not exist or cannot be read from
 		printf("Unable to access BLARG! source file '%s'", filename);
